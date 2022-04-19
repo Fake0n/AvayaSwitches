6ba63b21a5b26b9ad40dbdeb144d1b4e124cffac
Sergey <97732361+Fake0n@users.noreply.github.com>
2022-04-19 09:34:57 +0300

    Update work file .py

--- work file .py
+++ work file .py
@@ -50,6 +50,6 @@ if __name__ == "__main__":
     with open('path', 'r') as file: # Нужно указать полный путь к файлу С:/.../xxxx.txt
         lines = file.readlines()
         for devices in lines:
-            devices=devices.strip()[:11] # Нужно для того, чтобы при чтении файла удалить лишний символ переноса строки '\n', чтобы посдставлялся корректный IP.
+            devices=devices.strip()[:] # Нужно для того, чтобы при чтении файла удалить лишний символ переноса строки '\n', чтобы посдставлялся корректный IP.
             commands = ['sh clock','sh cli info'] # Здесь нужно указать нужные команды последовательно (так, как вводили бы вручную).
             result = send_show_command(f"{devices}", "login", "password", commands)
